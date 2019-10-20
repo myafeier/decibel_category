950108273e17314b999c799ca41564463fa077b8
Joffre <hiall@aliyun.com>
2019-10-20 16:25:42 +0800

    add get on

--- std_category.go
+++ std_category.go
@@ -1,6 +1,7 @@
 package decibel_category
 
 import (
+	"fmt"
 	"github.com/go-xorm/xorm"
 )
 
@@ -26,9 +27,20 @@ func (self *DefaultCategory)Delete(id int64)(err error){
 	return
 }
 func (self *DefaultCategory)Update(id int64,category *Category)(err error){
-	_,err=self.session.ID(id).Update(category)
+	_,err=self.session.ID(id).AllCols().Update(category)
 	return
 }
+func (self *DefaultCategory)Get(id int64)(result *Category,err error){
+	has,err:=self.session.ID(id).Get(result)
+	if err != nil {
+		return
+	}
+	if !has {
+		err=fmt.Errorf("invalid id")
+	}
+	return
+}
+
 func (self *DefaultCategory)GetChild(categoryType CateType,id int64)(result []*Category,err error){
 	session:=self.session.Where("cate_type=?",categoryType).And("state=?",StateOk).OrderBy("list_order")
 	if id>0{
